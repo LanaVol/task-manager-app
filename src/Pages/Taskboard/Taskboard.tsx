4f9c757f3473f1206718889361bbec025a9a49c3
LanaVol <svolosh4yk@gmail.com>
2023-05-11 18:42:00 +0300

    drag&drop/delete f localeStorage

--- src/Pages/Taskboard/Taskboard.tsx
+++ src/Pages/Taskboard/Taskboard.tsx
@@ -9,6 +9,10 @@ import TaskService from "../../services/TaskService";
 
 export const TaskBoard = () => {
   const [boards, setBoards] = useState<BoardItem[]>([]);
+  const [targetCard, setTargetCard] = useState({
+    boardId: 0,
+    cardId: 0,
+  });
 
   useEffect(() => {
     async function fetch() {
@@ -19,11 +23,6 @@ export const TaskBoard = () => {
     }
     fetch();
   }, []);
-
-  const [targetCard, setTargetCard] = useState({
-    boardId: 0,
-    cardId: 0,
-  });
 
   // adding new board
   const addBoardHandler = async (boardTitle: string) => {
@@ -45,12 +44,9 @@ export const TaskBoard = () => {
 
   // adding new card to current board
   const addCardHandler = async (boardId: number, cardTitle: string) => {
-    const boardIndex = boards.findIndex((el: BoardItem) => {
-      return el.id === boardId;
-    });
-    if (boardIndex < 0) {
-      return;
-    }
+    const boardIndex = boards.findIndex((el: BoardItem) => el.id === boardId);
+    if (boardIndex === -1) return;
+
     const tempBoardList = [...boards];
     tempBoardList[boardIndex].cards.push({
       id: Date.now() + Math.random() * 2,
@@ -59,6 +55,7 @@ export const TaskBoard = () => {
       date: "",
       tasks: [],
     });
+
     const { data } = await TaskService.updateBoard({
       boardId,
       board: tempBoardList[boardIndex],
@@ -78,9 +75,7 @@ export const TaskBoard = () => {
     const boardIndex = boards.findIndex((el: BoardItem) => {
       return el.id === boardId;
     });
-    if (boardIndex < 0) {
-      return;
-    }
+    if (boardIndex === -1) return;
 
     const updatedBoard = { ...boards[boardIndex] };
 
@@ -111,17 +106,15 @@ export const TaskBoard = () => {
     const boardIndex = boards.findIndex((el) => {
       return el.id === boardId;
     });
-    if (boardIndex < 0) {
-      return;
-    }
+    if (boardIndex === -1) return;
+
     const updatedBoard = { ...boards[boardIndex] };
     const cards = updatedBoard.cards;
     const cardIndex = cards.findIndex((el) => {
       return el.id === cardId;
     });
-    if (cardIndex < 0) {
-      return;
-    }
+    if (cardIndex === -1) return;
+
     updatedBoard.cards[cardIndex] = card;
 
     const { data } = await TaskService.updateBoard({
@@ -138,58 +131,72 @@ export const TaskBoard = () => {
   };
 
   // drag&drop cards
-  const onDragEnd = (boardId: number, cardId: number) => {
-    // const sourceBoardIndex = boards.findIndex((el: BoardItem) => {
-    //   return el.id === boardId;
-    // });
-    // if (sourceBoardIndex < 0) {
-    //   return;
-    // }
-    // const sourceCardIndex = boards[sourceBoardIndex].cards?.findIndex(
-    //   (el: CardItem) => {
-    //     return el.id === cardId;
-    //   }
-    // );
-    // if (sourceCardIndex < 0) {
-    //   return;
-    // }
-    // const targetBoardIndex = boards.findIndex((el: BoardItem) => {
-    //   return el.id === targetCard.boardId;
-    // });
-    // if (targetBoardIndex < 0) {
-    //   return;
-    // }
-    // const targetCardIndex = boards[targetBoardIndex].cards?.findIndex(
-    //   (el: CardItem) => {
-    //     return el.id === targetCard.cardId;
-    //   }
-    // );
-    // if (targetCardIndex < 0) {
-    //   return;
-    // }
-    // const tempBoardList = [...boards];
-    // const sourceCard = tempBoardList[sourceBoardIndex].cards[sourceCardIndex];
-    // tempBoardList[sourceBoardIndex].cards.splice(sourceCardIndex, 1);
-    // tempBoardList[targetBoardIndex].cards.splice(
-    //   targetCardIndex,
-    //   0,
-    //   sourceCard
-    // );
-    // setBoards(tempBoardList);
-    // setTargetCard({ boardId: 0, cardId: 0 });
+  const onDragEnd = async (boardId: number, cardId: number) => {
+    const sourceBoardIndex = boards.findIndex((el: BoardItem) => {
+      return el.id === boardId;
+    });
+    if (sourceBoardIndex === -1) return;
+
+    const sourceCardIndex = boards[sourceBoardIndex].cards?.findIndex(
+      (el: CardItem) => {
+        return el.id === cardId;
+      }
+    );
+    if (sourceCardIndex === -1) return;
+
+    const targetBoardIndex = boards.findIndex(
+      (el: BoardItem) => el.id === targetCard.boardId
+    );
+
+    if (targetBoardIndex === -1) return;
+
+    const targetCardIndex = boards[targetBoardIndex].cards?.findIndex(
+      (el: CardItem) => el.id === targetCard.cardId
+    );
+    if (targetCardIndex === -1) return;
+
+    const tempBoardList = [...boards];
+
+    const sourceCard = tempBoardList[sourceBoardIndex].cards[sourceCardIndex];
+    tempBoardList[sourceBoardIndex].cards.splice(sourceCardIndex, 1);
+    tempBoardList[targetBoardIndex].cards.splice(
+      targetCardIndex,
+      0,
+      sourceCard
+    );
+
+    const boardDel = tempBoardList[sourceBoardIndex];
+    const boardAdded = tempBoardList[targetBoardIndex];
+
+    const [board1, board2] = await Promise.all([
+      TaskService.updateBoard({
+        boardId: boardDel.id,
+        board: boardDel,
+      }),
+      TaskService.updateBoard({
+        boardId: boardAdded.id,
+        board: boardAdded,
+      }),
+    ]);
+
+    const updateBoardList = boards.map((board) => {
+      if (board.id === board1.data.id) {
+        return board1.data;
+      }
+      if (board.id === board2.data.id) {
+        return board2.data;
+      }
+      return board;
+    });
+
+    setBoards(updateBoardList);
+    setTargetCard({ boardId: 0, cardId: 0 });
   };
 
   const onDragEnter = (boardId: number, cardId: number) => {
-    // console.log("dragEnter");
-    // if (targetCard.cardId === cardId) {
-    //   return;
-    // }
-    // setTargetCard({ boardId: boardId, cardId: cardId });
+    if (targetCard.cardId === cardId) return;
+    setTargetCard({ boardId: boardId, cardId: cardId });
   };
-
-  // useEffect(() => {
-  //   updateLocaleStorageBoards(boards);
-  // }, [boards]);
 
   return (
     <Box
