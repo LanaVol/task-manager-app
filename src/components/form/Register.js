049d50fde67c6b31def6c812369168d1dc35ddc9
LanaVol <svolosh4yk@gmail.com>
2023-05-14 14:56:42 +0300

    blur/basicValid before Yup

--- src/components/form/Register.js
+++ src/components/form/Register.js
@@ -54,9 +54,12 @@ export const SignupForm = () => {
           name="firstName"
           type="text"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.firstName}
         />
-        {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+        {formik.touched.firstName && formik.errors.firstName ? (
+          <div>{formik.errors.firstName}</div>
+        ) : null}
       </div>
 
       <div>
@@ -66,9 +69,12 @@ export const SignupForm = () => {
           name="lastName"
           type="text"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.lastName}
         />
-        {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+        {formik.touched.lastName && formik.errors.lastName ? (
+          <div>{formik.errors.lastName}</div>
+        ) : null}
       </div>
 
       <div>
@@ -78,9 +84,12 @@ export const SignupForm = () => {
           name="email"
           type="text"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.email}
         />
-        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        {formik.touched.lastName && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
       </div>
 
       <button type="submit">Submit</button>
