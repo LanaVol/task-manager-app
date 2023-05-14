313cc598c196427e989e5123fdf2dbd076bfd531
LanaVol <svolosh4yk@gmail.com>
2023-05-14 14:47:19 +0300

    formAuth/Formik

--- src/pages/Register/Auth.js
+++ src/pages/Register/Auth.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { SignupForm } from "../../components/form/Register";
 
 export const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <div style={{ padding: "20px", display: "flex", flexDirection: "column" }}>
+      <SignupForm />
+    </div>
+  );
 };
