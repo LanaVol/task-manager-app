95cfc02c9c2f81a49cb05f0e277d674e9e957eb3
LanaVol <svolosh4yk@gmail.com>
2023-08-20 17:53:30 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import { Header } from "./components/Header/Header";
